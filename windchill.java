80d63d7e01be9c91784c207471f5090456cc4e62
Parvsub <Praveenboda421@gmail.com>
2023-02-07 14:12:16 +0530

    Added: Condition for temperature and windspeed updated

--- windchill.java
+++ windchill.java
@@ -12,9 +12,12 @@ public class windchill {
         System.out.println("Enter wind speed: ");
         windspeed = scanner.nextDouble();
         // Calculate wind chill
-        double windChill = 35.74 + 0.6215 * temperature +
-                (0.4275 * temperature - 35.75) * Math.pow(windspeed, 0.16);
-        System.out.println("Wind chill is: " + windChill);
+        if (temperature > 50 || windspeed > 120 || windspeed < 3) {
+            System.out.println("Invalid");
+        } else {
+            double windChill = 35.74 + (0.6215 * temperature) +
+                    ((0.4275 * temperature - 35.75) * Math.pow(windspeed, 0.16));
+            System.out.println("Wind chill is: " + windChill);
+        }
     }
 }
-
